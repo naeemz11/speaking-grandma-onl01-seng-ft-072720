93ca60daac6920b41f858770c806cdeac7188c93
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-28 14:54:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,11 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     
-    elsif phrase == phrase .downcase 
+    elsif phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+    
+    elsif phrase == phrase.upcase 
+    return "NO, NOT SINCE 1938!"
+    
   
 end
